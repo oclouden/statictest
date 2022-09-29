66bcc13b1ec5ce89630e9c943cafc3097a6b87f6
oclouden <loudenowen@gmail.com>
2022-09-29 14:14:55 -0400

    try again

--- demo 2/statictest/src/App.js
+++ demo 2/statictest/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> nd save to reload.
+          Edit <code>src/App.js</code> nd .
         </p>
         <a
           className="App-link"
